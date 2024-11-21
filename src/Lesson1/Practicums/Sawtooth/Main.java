83fa029147fd5d98399f7ea5240ce9ca156f502b
slmbakkenes <slmbakkenes@gmail.com>
2024-11-21 10:52:45 +0100

    Updated sawtooth practicum

--- src/Lesson1/Practicums/Sawtooth/Main.java
+++ src/Lesson1/Practicums/Sawtooth/Main.java
@@ -2,11 +2,16 @@ package Lesson1.Practicums.Sawtooth;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i <= 5) {
-                System.out.print("s");
+        int rows = 4;
+
+        String text = "s";
+
+        for (int i = 1; i <= rows; i++) {
+            System.out.println(text);
+            if (i % 2 == 0) {
+                text = "s";
             } else {
-                System.out.print("t");
+                text += "s";
             }
         }
     }
